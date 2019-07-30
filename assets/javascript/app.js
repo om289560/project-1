5e34482d1766bac0820d5b66cdbb736b83486fb7
Abdullah Bayindir <byndrabdullah@gmail.com>
2019-07-29 20:30:08 -0500

    embedded youtube video, working search display

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,13 +7,15 @@ $(document).ready(function () {
     });
     
     function youtubeQuery(songName) {
-        var youTubeURL = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&channelId=UC2pmfLm7iq6Ov1UwYrWYkZA&q=" + songName + "&key=AIzaSyBrUzOmwgzmZPFQ6rfWBY8-SyUp1C9LZ8Y";
+        var youTubeURL = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=vevo%20" + songName + "&VideoEmbedded=true&key=AIzaSyBrUzOmwgzmZPFQ6rfWBY8-SyUp1C9LZ8Y";
 
         $.ajax({
             url: youTubeURL
+            
         }).then( function (response) {
             console.log(response);
-            // $("#youtube-content").html("<iframe id='ytplayer' type='text/html' width='640' height='360' src=https://www.youtube.com/embed/M7lc1UVf-VE?autoplay=1&origin=http:" + youTubeURL + ">");
+            var videoID = response.items[0].id.videoId
+            $("#youtube-content").append("<iframe id='ytplayer' type='text/html' width='640' height='360' src='https://www.youtube.com/embed/"+ videoID + "?autoplay=1&origin=http:" + videoID + "?version=3' frameborder='0'></iframe>");
         });
     }
 
